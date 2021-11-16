69776465cbc84d7e4f4cc8c93d90b87291bc11d3
Taha Sahin <www.taha-@hotmail.com>
2021-11-16 21:13:50 +0300

    Testing everything is rigth

--- Source.py
+++ Source.py
@@ -1,1 +1,10 @@
-print("hello world ")
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+
+s=Service('C:\Program Files (x86)\chromedriver.exe')
+driver = webdriver.Chrome(service=s)
+url='https://www.google.com'
+driver.get(url)
+
+search = driver.find_element_by_name("q")
+search.send_keys("test")
